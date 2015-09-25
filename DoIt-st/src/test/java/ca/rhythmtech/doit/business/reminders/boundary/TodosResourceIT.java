f9d612dc496f04d38acbdc79c91ded3deaf9e221
Mark Doucette <mark@rhythmtech.ca>
2015-09-24 19:18:17 -0700

    First system test for ToDos

--- DoIt-st/src/test/java/ca/rhythmtech/doit/business/reminders/boundary/TodosResourceIT.java
+++ DoIt-st/src/test/java/ca/rhythmtech/doit/business/reminders/boundary/TodosResourceIT.java
@@ -1,12 +1,19 @@
 package ca.rhythmtech.doit.business.reminders.boundary;
 
+import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Test the TodosResource class
@@ -14,17 +21,16 @@ import javax.ws.rs.core.MediaType;
  */
 public class TodosResourceIT {
 
-    private Client client;
-    private WebTarget tut;
-
-    @Before
-    public void initClient() {
-        this.client = ClientBuilder.newClient();
-        this.tut = this.client.target("http://localhost:8080/DoIt/api/todos/");
-    }
+    @Rule
+    public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("http://localhost:8080/DoIt/api/todos/");
 
     @Test
     public void fetchTodos() {
-                this.tut.request(MediaType.TEXT_PLAIN_TYPE);
+        Response response = this.provider.target().request(MediaType.APPLICATION_JSON).get();
+        assertThat(response.getStatus(), is(200));
+
+        String payload = response.readEntity(String.class);
+        assertTrue(payload.contains(""));
+
     }
 }
