f9c0cab90b565324618ca8e37f903a7486b5fec6
Mark Doucette <mark@rhythmtech.ca>
2015-09-24 21:15:28 -0700

    Finish system test for TodoResource

--- DoIt-st/src/test/java/ca/rhythmtech/doit/business/reminders/boundary/TodosResourceIT.java
+++ DoIt-st/src/test/java/ca/rhythmtech/doit/business/reminders/boundary/TodosResourceIT.java
@@ -1,17 +1,17 @@
 package ca.rhythmtech.doit.business.reminders.boundary;
 
 import com.airhacks.rulz.jaxrsclient.JAXRSClientProvider;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonValue;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -20,7 +20,6 @@ import static org.junit.Assert.assertTrue;
  * @author Mark Doucette
  */
 public class TodosResourceIT {
-
     @Rule
     public JAXRSClientProvider provider = JAXRSClientProvider.buildWithURI("http://localhost:8080/DoIt/api/todos/");
 
@@ -29,8 +28,32 @@ public class TodosResourceIT {
         Response response = this.provider.target().request(MediaType.APPLICATION_JSON).get();
         assertThat(response.getStatus(), is(200));
 
-        String payload = response.readEntity(String.class);
-        assertTrue(payload.contains(""));
+        JsonArray allTodos = response.readEntity(JsonArray.class);
+        System.out.println(allTodos);
+        System.out.println("Size of allTodos: " + allTodos.size());
+        assertFalse(allTodos.isEmpty());
 
+        JsonObject todo = allTodos.getJsonObject(3);
+        System.out.println(todo);
+        assertTrue(todo.getString("caption").contains("REST"));
+    }
+
+    @Test
+    public void fetchTodo() {
+        JsonObject todo = this.provider.target()
+                .path("42")
+                .request(MediaType.APPLICATION_JSON)
+                .get(JsonObject.class);
+        assertTrue(todo.getString("caption").contains("42"));
+        System.out.println("Fetch todo: " + todo);
+    }
+
+    @Test
+    public void deleteTodo()  {
+        Response deleteResponse = this.provider.target()
+                .path("42")
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+        assertThat(deleteResponse.getStatus(), is(204));
     }
 }
